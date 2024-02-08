c48d6ce7638325164602202c62d437810af897ed
Gabriela Cressa <cressagabriela@gmail.com>
2024-02-08 12:26:53 +0100

    calculadora terminada

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calculadora';
+  display='0';
+
+  clearDisplay(){
+    this.display='0';
+  }
+
+  appendcharacter(character:string){
+    this.display+=character;
+
+  }
+ calculate() {
+    try {
+      this.display = eval(this.display);
+    } catch (error) {
+      this.display = 'Error';
+
 }
+
+
+
+}}
